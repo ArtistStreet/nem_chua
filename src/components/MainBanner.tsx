fc3abf46e311487348059d1e450dc6421bd9f4ef
ArtistStreet <darkstarcfvn@gmail.com>
2025-11-19 21:50:29 +0700

    Update HomePage

---
+++ src/components/MainBanner.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+type MainBannerProps = {
+  mainTitle: string;
+  subTitle: string;
+};
+
+const MainBanner: React.FC<MainBannerProps> = ({ mainTitle, subTitle }) => {
+  return (
+    <>
+      <section
+        className="py-5 text-center position-relative overflow-hidden"
+        style={{
+          background:
+            "linear-gradient(rgba(0,100,0,0.7), rgba(0,100,0,0.8)), url('/images/nem-chua.jpg.webp') center/cover no-repeat",
+          minHeight: "400px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="container position-relative z-1">
+          <h1 className="display-5 fw-bold text-white mb-3">{mainTitle}</h1>
+          <p className="text-white-50 fs-5">
+            <Link to="/" className="text-white-50 text-decoration-none">
+              Trang chủ
+            </Link>
+            <i className="bi bi-chevron-right mx-2"></i>
+            {subTitle}
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MainBanner;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,6 @@
 // src/pages/Products/ProductsPage.tsx
 import { Link } from "react-router-dom";
+import MainBanner from "../components/MainBanner";
 
 export default function Products() {
   const products = [
@@ -23,28 +24,10 @@ export default function Products() {
 
   return (
     <>
-      <section
-        className="py-5 text-center position-relative overflow-hidden"
-        style={{
-          background:
-            "linear-gradient(rgba(0,100,0,0.7), rgba(0,100,0,0.8)), url('/images/nem-chua.jpg.webp') center/cover no-repeat",
-          minHeight: "400px",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <div className="container position-relative z-1">
-          <h1 className="display-5 fw-bold text-white mb-3">Sản phẩm</h1>
-          <p className="text-white-50 fs-10">
-            Trang chủ <i className="bi bi-chevron-right mx-2"></i> Sản phẩm
-          </p>
-        </div>
-      </section>
+      <MainBanner mainTitle="Sản Phẩm" subTitle="Products" />
 
-      {/* Danh sách sản phẩm */}
       <section className="py-5 bg-light">
         <div className="container">
-          {/* Grid sản phẩm */}
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
             {products.map((product, index) => (
               <div className="col" key={index}>
