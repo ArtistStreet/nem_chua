687d64cdbaef9ca72f52db311376001071c5643f
ArtistStreet <darkstarcfvn@gmail.com>
2025-11-20 13:37:39 +0700

    Update Product

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,27 +1,8 @@
-// src/pages/Products/ProductsPage.tsx
 import { Link } from "react-router-dom";
 import MainBanner from "../components/MainBanner";
+import { products } from "../data/products";
 
 export default function Products() {
-  const products = [
-    {
-      name: "Nem Chua",
-      img: "/images/nem-chua.jpg.webp",
-    },
-    {
-      name: "Nem Cói",
-      img: "/images/nem-coi.jpg",
-    },
-    {
-      name: "Nem Nướng",
-      img: "/images/nem-nuong.jpg.webp",
-    },
-    {
-      name: "Nem Thính",
-      img: "/images/nem-thinh.jpg.webp",
-    },
-  ];
-
   return (
     <>
       <MainBanner mainTitle="Sản Phẩm" subTitle="Products" />
@@ -32,7 +13,7 @@ export default function Products() {
             {products.map((product, index) => (
               <div className="col" key={index}>
                 <Link
-                  to={`/san-pham/${product.name
+                  to={`/san-pham/${product.slug
                     .toLowerCase()
                     .replace(/ /g, "-")}`}
                   className="text-decoration-none"
@@ -40,14 +21,14 @@ export default function Products() {
                   <div className="card h-100 border-0 shadow-sm hover-shadow transition">
                     <div className="ratio ratio-1x1">
                       <img
-                        src={product.img}
+                        src={product.image}
                         className="card-img-top rounded-top"
                         alt={product.name}
                         style={{ objectFit: "cover" }}
                       />
                     </div>
                     <div className="card-body text-center py-4">
-                      <h5 className="card-title text-dark mb-0 fw-bold">
+                      <h5 className="card-title text-success mb-0 fw-bold">
                         {product.name}
                       </h5>
                     </div>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/About";
 import ProductsPage from "../pages/Products";
+import ProductDetailPage from "../pages/ProductDetailPage";
 // import NewsPage from "../pages/News/NewsPage";
 // import ContactPage from "../pages/Contact/ContactPage";
 
@@ -11,6 +12,7 @@ export default function AppRoutes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/gioi-thieu" element={<AboutPage />} />
       <Route path="/san-pham" element={<ProductsPage />} />
+      <Route path="/san-pham/:slug" element={<ProductDetailPage />} />
       {/* <Route path="/tin-tuc" element={<NewsPage />} /> */}
       {/* <Route path="/lien-he" element={<ContactPage />} /> */}
     </Routes>
